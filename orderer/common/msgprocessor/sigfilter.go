c59d5469d819a8e4d4a479a97b64af85dc97cc2b
Basil K Y <ky.basil@tcs.com>
2020-02-06 12:18:11 +0530

    signature verification by indy at orderer

--- orderer/common/msgprocessor/sigfilter.go
+++ orderer/common/msgprocessor/sigfilter.go
@@ -7,8 +7,14 @@ SPDX-License-Identifier: Apache-2.0
 package msgprocessor
 
 import (
+	"bytes"
+	"crypto/sha256"
+	b64 "encoding/base64"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/hyperledger/fabric/common/channelconfig"
 	"github.com/hyperledger/fabric/common/policies"
 	cb "github.com/hyperledger/fabric/protos/common"
@@ -47,6 +53,49 @@ func NewSigFilter(normalPolicyName, maintenancePolicyName string, support SigFil
 
 // Apply applies the policy given, resulting in Reject or Forward, never Accept
 func (sf *SigFilter) Apply(message *cb.Envelope) error {
+
+	payload := &cb.Payload{}
+	err := proto.Unmarshal(message.Payload, payload)
+	if err != nil {
+		return fmt.Errorf("Failed unmarshaling payload")
+	}
+
+	if payload.Header == nil /* || payload.Header.SignatureHeader == nil */ {
+		return fmt.Errorf("Failed getting header")
+	}
+	shdr := &cb.SignatureHeader{}
+	err = proto.Unmarshal(payload.Header.SignatureHeader, shdr)
+	if err != nil {
+		return fmt.Errorf("GetSignatureHeaderFromBytes failed, err %s", err)
+	}
+	if shdr.Did != nil {
+		fmt.Println("received indy signed proposal, verifying by indy")
+		hash := sha256.Sum256(message.Payload)
+		encoded := b64.StdEncoding.EncodeToString(hash[:])
+		fmt.Println()
+		fmt.Println()
+		fmt.Println("calculated hash", hash)
+		fmt.Println("encoded hash", encoded)
+
+		url := "http://10.53.17.40:8003/verify_signature"
+
+		payload := []byte("{\"message\" : \"" + encoded + "\",\"their_did\" : \"" + string(shdr.Did) + "\",\"signature\": \"" + string(message.Signature) + "\"}")
+		fmt.Println("prepared payload", string(payload))
+		req, _ := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+
+		req.Header.Add("content-type", "text/plain")
+
+		res, _ := http.DefaultClient.Do(req)
+
+		defer res.Body.Close()
+		body, _ := ioutil.ReadAll(res.Body)
+		fmt.Println("response received", body)
+		fmt.Println("stringified response", string(body))
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		return nil
+	}
 	ordererConf, ok := sf.support.OrdererConfig()
 	if !ok {
 		logger.Panic("Programming error: orderer config not found")
