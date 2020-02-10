f10c5e3436292b7bbe04c3a21b243cb21eb072f2
Basil K Y <ky.basil@tcs.com>
2020-02-10 14:58:16 +0530

    log

--- core/common/validation/msgvalidation.go
+++ core/common/validation/msgvalidation.go
@@ -244,7 +244,7 @@ func validateSignatureHeader(sHdr *common.SignatureHeader) error {
 	// ensure that there is a creator
 	if sHdr.Creator == nil || len(sHdr.Creator) == 0 {
 		if sHdr.Did == nil || len(sHdr.Did) == 0 {
-			return errors.New("invalid creator specified in the header" + string(sHdr.Did) + string(sHdr.Creator))
+			return errors.New("invalid creator specified in the header" + string(sHdr.Did) + string(sHdr.Creator) + "done")
 		}
 	}
 
