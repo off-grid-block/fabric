9646378ab5a40bc468980314ce29a58678e25815
Basil K Y <ky.basil@tcs.com>
2020-02-28 18:09:39 +0530

    update indyverify function

--- common/indyverify/indyverify.go
+++ common/indyverify/indyverify.go
@@ -16,16 +16,8 @@ type verifySignatureOut struct {
 	Status        string `json:"status"`
 	Connection_id string `json:"connection_id"`
 }
-type attributes struct {
-	App_name string `json:"app_name"`
-	App_id   string `json:"app_id"`
-}
-type verifyProofOut struct {
-	Status     string     `json:"status"`
-	Attributes attributes `json:"attributes"`
-}
 
-func Indyverify(ProposalBytes []byte, Did []byte, Signature []byte) (status bool, err error) {
+func Indyverify(ProposalBytes []byte, did []byte, signature []byte) (status bool, err error, id string) {
 
 	hash := sha256.Sum256(ProposalBytes)
 	encoded := b64.StdEncoding.EncodeToString(hash[:])
@@ -33,18 +25,27 @@ func Indyverify(ProposalBytes []byte, Did []byte, Signature []byte) (status bool
 	fmt.Println()
 	fmt.Println("calculated hash", hash)
 	fmt.Println("encoded hash", encoded)
-
+	type Payload struct {
+		Message   string `json:"message"`
+		Did       string `json:"their_did"`
+		Signature string `json:"signature"`
+	}
 	url := "http://10.53.17.40:8003/verify_signature"
-	payload := []byte("{\"message\" : \"" + encoded + "\",\"their_did\" : \"" + string(Did) + "\",\"signature\": \"" + string(Signature) + "\"}")
-	fmt.Println("prepared payload", string(payload))
-	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	payload := Payload{Message: encoded, Did: string(did), Signature: string(signature)}
+	payloadbytes, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("prepared payload", payload)
+	payloadbytesstring := string(payloadbytes)
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer([]byte(payloadbytesstring)))
 
 	req.Header.Add("content-type", "text/plain")
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Println("Received error from Indy server", err)
-		return false, errors.New("Error connecting to Indy server")
+		return false, errors.New("Error connecting to Indy server"), ""
 	}
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
@@ -53,22 +54,22 @@ func Indyverify(ProposalBytes []byte, Did []byte, Signature []byte) (status bool
 
 	fmt.Println(string(body))
 
-	outJson := verifySignatureOut{}
-	err = json.Unmarshal(body, &outJson)
+	respJson := verifySignatureOut{}
+	err = json.Unmarshal(body, &respJson)
 	if err != nil {
 		fmt.Println("error unmarshaling response from Indy", err)
-		return false, errors.New("error unmarshaling response from Indy")
+		return false, errors.New("error unmarshaling response from Indy"), ""
 	}
 
-	if outJson.Status != "Signature verified" {
-		return false, errors.New(outJson.Status)
+	if respJson.Status != "Signature verified" {
+		return false, errors.New(respJson.Status), ""
 	}
 	// Verify proof code
 	/*
 		attrib := "app_name,app_id"
 		an := "voter"
 		ai := "101"
-		conn_Id := outJson.Connection_id
+		conn_Id := respJson.Connection_id
 		url = "http://10.53.17.40:8003/verify_proof"
 		payload = []byte("{\"proof_attr\" : \"" + attrib + "\",\"connection_id\" : \"" + conn_Id + "\"}")
 
@@ -85,23 +86,23 @@ func Indyverify(ProposalBytes []byte, Did []byte, Signature []byte) (status bool
 		defer res.Body.Close()
 		body, _ = ioutil.ReadAll(res.Body)
 
-		verifyOutJson := verifyProofOut{}
-		err = json.Unmarshal(body, &verifyOutJson) //unmarshal it aka JSON.parse()
+		verifyrespJson := verifyProofOut{}
+		err = json.Unmarshal(body, &verifyrespJson) //unmarshal it aka JSON.parse()
 		if err != nil {
 			fmt.Println(err)
 		}
 
-		fmt.Println("response received", verifyOutJson)
+		fmt.Println("response received", verifyrespJson)
 		fmt.Println("stringified response", string(body))
-		fmt.Println(verifyOutJson.Status)
+		fmt.Println(verifyrespJson.Status)
 
-		if verifyOutJson.Status != "True" {
+		if verifyrespJson.Status != "True" {
 			return nil, nil, nil, errors.Errorf("Attributes missing !!!")
 		}
 
-		if !(verifyOutJson.Attributes.App_name == an && verifyOutJson.Attributes.App_id == ai) {
+		if !(verifyrespJson.Attributes.App_name == an && verifyrespJson.Attributes.App_id == ai) {
 			return nil, nil, nil, errors.Errorf("Attribute values didnt match")
 		}
 	*/
-	return true, nil
+	return true, nil, respJson.Connection_id
 }
